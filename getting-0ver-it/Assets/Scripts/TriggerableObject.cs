ccd11caa6fc8d964f8ea02d92eb6e4bfc9f8fabc
b4ttl3C4t <sam00298653@gmail.com>
2025-04-27 15:09:57 +0800

    feat: add respawn rule and its boundary object

--- getting-0ver-it/Assets/Scripts/TriggerableObject.cs
+++ getting-0ver-it/Assets/Scripts/TriggerableObject.cs
@@ -6,6 +6,7 @@ public class TriggerableObject : MonoBehaviour
 {
     [Header("2D-triggerable object")]
     public int score = 1;
+    private Animator animator;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,14 +14,19 @@ public class TriggerableObject : MonoBehaviour
         if(collision.CompareTag("Player"))
         {
             ScoreManager.instance.AddScore(score);
-            Destroy(gameObject);
+
+            // play the animation
+            animator.SetTrigger("CoinsPickup");
+
+            // delay 1 second to play the animation
+            Destroy(gameObject, 3f);
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
